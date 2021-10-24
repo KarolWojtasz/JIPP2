26d58095dd9f047bf935062cff8716b88f62f94f
user <karol.wojtasz@student.pk.edu.pl>
2021-10-24 14:01:47 +0200

    firstLib first commit

---
+++ firstLib/src/lib.cpp
@@ -0,0 +1,9 @@
+//
+// Created by user on 19.10.2021.
+//
+#include "../include/lib.h"
+#include <iostream>
+using namespace std;
+void itIsWork(){
+    cout<<"it is work";
+}
---
+++ firstLibCall/src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by user on 19.10.2021.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    itIsWork();
+    return 0;
+}
